7d68dd016e022900a07dac3ca240f7d5595f5347
justinpost <justin.m.post@gmail.com>
2014-02-27 20:11:59 +0100

    weather

---
+++ js/location.js
@@ -0,0 +1,44 @@
+// JavaScript Document
+// Location functions
+
+function getCurrentLocation(){
+	navigator.geolocation.getCurrentPosition(
+											 function(position){
+												 var teruggeven = {	"lat": position.coords.latitude,
+												 					"long": position.coords.longitude,
+																	"speed": position.coords.speed};
+												return teruggeven
+												 },
+											 function(error){return null;});
+}
+
+function getFutureLocation(){
+	// get next calendar entry
+	var startDate = new Date(2014,2,15,18,30,0,0,0); // beware: month 0 = january, 11 = december
+	var endDate = new Date(2014,2,15,19,30,0,0,0);
+	var title = "My nice event";
+	var location = "Home";
+	var notes = "Some notes about this event.";
+	var success = function(message) { alert("Success: " + JSON.stringify(message)); };
+	var error = function(message) { alert("Error: " + message); };
+	
+	window.plugins.calendar.findEvent(title,location,notes,startDate,endDate,success,error);
+	/*if(){		// is entry, get that location
+		
+	} else {	// if not, go home
+		// where is home?
+	}*/
+}
+
+function getWeather(lat, long){
+	/*$.ajax({
+	  url: 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long,
+	  success: function(data){weer = data;}
+	});*/
+	var test = 10;
+	$.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long,
+					 function(data){
+						 	var dataToStore = JSON.stringify(data);
+						 	localStorage.setItem('weer', dataToStore);
+						 });
+}
